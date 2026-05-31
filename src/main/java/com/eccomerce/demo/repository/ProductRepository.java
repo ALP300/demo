ff1fb09dd30f199beacc88c80a080c1634770c4a
ALP300 <aitorpalomino1@gmail.com>
2026-05-30 19:52:45 -0500

    a

--- src/main/java/com/eccomerce/demo/repository/ProductRepository.java
+++ src/main/java/com/eccomerce/demo/repository/ProductRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.eccomerce.demo.model.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    
+
 }
